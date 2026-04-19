c2197cdbf6537bd2940ed41b32f07c581e9ca35e
Sam Mumford <smmmfrd@gmail.com>
2026-04-19 10:47:06 -0700

    Get Feeds and User Data Query

--- src/db/queries/feeds.ts
+++ src/db/queries/feeds.ts
@@ -1,5 +1,6 @@
-import { feeds } from "src/schema";
+import { feeds, users } from "src/schema";
 import { db } from "..";
+import { eq } from "drizzle-orm";
 
 export async function createFeed(name: string, url: string, user_id: string) {
   const [result] = await db
@@ -7,4 +8,16 @@ export async function createFeed(name: string, url: string, user_id: string) {
     .values({ name, url, user_id })
     .returning();
   return result;
+}
+
+export async function getFeeds() {
+  const results = await db
+    .select({
+      userName: users.name,
+      url: feeds.url,
+      name: feeds.name,
+    })
+    .from(feeds)
+    .leftJoin(users, eq(feeds.user_id, users.id));
+  return results;
 }
